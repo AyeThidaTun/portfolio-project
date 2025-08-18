24fc4411c07e6e741c9a94cd2c721fba922d25af
AyeThidaTun <ayethidartun.eduvalor.2023d@gmail.com>
2025-08-18 13:45:29 +0800

    fix: fix mobile view

--- src/app/_components/hero-section.tsx
+++ src/app/_components/hero-section.tsx
@@ -9,7 +9,7 @@ const images = [
   { src: "/pink.svg", alt: "pink", size: 60, top: "8vh", left: "80vw" },
   { src: "/blue.svg", alt: "blue", size: 60, top: "60vh", left: "70vw" },
   { src: "/pink.svg", alt: "pink", size: 60, top: "90vh", left: "10vw" },
-  { src: "/blue.svg", alt: "draggable", size: 60, top: "40vh", left: "5vw" },
+  { src: "/blue.svg", alt: "draggable", size: 60, top: "25vh", left: "5vw" },
   { src: "/pink.svg", alt: "pink", size: 60, top: "80vh", left: "80vw" },
   { src: "/blue.svg", alt: "blue", size: 60, top: "70vh", left: "30vw" },
 ];
